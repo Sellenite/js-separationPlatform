ad02449eb637e026920e492ab0958b33d135f215
Sellenite <714627528@qq.com>
2017-11-09 21:13:57 +0800

    use ssh

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,9 +2,11 @@
  * @Author: Sellenite 
  * @Date: 2017-11-03 20:10:08 
  * @Last Modified by: Sellenite
- * @Last Modified time: 2017-11-09 19:57:52
+ * @Last Modified time: 2017-11-09 21:13:33
  */
 var util = require('util/util.js')
 require('page/common/nav-simple/index.js')
 require('page/common/nav/index.js')
 require('page/common/header/index.js')
+/* use ssh */
+var a = 123
