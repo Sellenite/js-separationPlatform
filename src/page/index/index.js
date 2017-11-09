a121a1c49d2e8ed977b077822bd439f530cd673b
Sellenite <714627528@qq.com>
2017-11-09 21:16:01 +0800

    fix use ssh push

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,11 +2,9 @@
  * @Author: Sellenite 
  * @Date: 2017-11-03 20:10:08 
  * @Last Modified by: Sellenite
- * @Last Modified time: 2017-11-09 21:13:33
+ * @Last Modified time: 2017-11-09 21:15:44
  */
 var util = require('util/util.js')
 require('page/common/nav-simple/index.js')
 require('page/common/nav/index.js')
 require('page/common/header/index.js')
-/* use ssh */
-var a = 123
